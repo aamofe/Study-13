fc401355cbd283ba449258175dd8a039c846fba5
qingzhang21 <2362339179git>
2023-03-13 14:14:48 +0800

    conflict solved

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 import os
 os.system('python function_20377101.py')
-=======
-import os
-os.system('python function_20377071.py')
->>>>>>> ae3164ca05b566ed540c8c0546a5320eabedefcc
-
-print("hello,world")
